a4ab65bd03b3048a63855aa7afd8709ebfcbac2f
Kunipatootie <60041611+Kunipatootie@users.noreply.github.com>
2021-05-30 18:50:58 -0400

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,9 +1,15 @@
 import yaml 
 
-if __name__ == '__main__':                          #
-
-    stream = open("data/example.yaml", 'r')                 #opens yaml file so that we can read from it
+def foo(file_path):
+    stream = open(file_path, 'r')                 #opens yaml file so that we can read from it
     dictionary = yaml.load(stream)                          #read from the file
 
     for key, value in dictionary.items():
         print(key + " : " + str(value))
+    
+    return dictionary
+
+
+if __name__ == '__main__':                          #
+
+    data = foo("data/example.yaml")
